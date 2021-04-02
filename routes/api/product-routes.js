fbd5d360376f07255153c10570f5622170c5b762
Wes Howard <BigRonald42@hotmail.com>
2021-04-01 18:51:14 -0700

    updated product delete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -20,14 +20,11 @@ router.get('/:id', async (req, res) => {
     order: ['product_name'],
     where: {
       
-      id: res.body.id
+      id: res.body.id,
     },
-    attributes: {
-      //attributes here...none?
-    }
-  }).then((pData) => {
+  })
     res.json(pData);
-  });
+  ;
 });
 
 // create new product
@@ -104,8 +101,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const proData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!catData) {
+      res.status(404).json({ message: 'No Product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(proData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
